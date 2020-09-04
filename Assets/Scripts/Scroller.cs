2d5e0560439fe7ff3769fd0926901ccb8bab39d4
Alberto Ramos <aramos@egogames.com>
2020-09-05 00:07:51 +0200

    Animation done for boat, background set and looping , changed canvas location and size, code for the zooming of the boat as game progresses (unfinished)

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    // Scroll the main texture based on time
+
+    public float m_speed = 0.5f;
+    private Renderer m_renderer;
+
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        m_renderer.material.SetTextureOffset("_MainTex", new Vector2(Time.time * m_speed, 0));
+    }
+}
